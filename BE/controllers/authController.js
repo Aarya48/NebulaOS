ec3dbd2b895541878b2a355e4844aaa8c004c611
Aarya Tiwari <tiwariaarya84@gmail.com>
2026-06-04 02:07:21 +0530

    basic

---
+++ BE/controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+ const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+module.exports = { registerUser };
---
+++ BE/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        wallpaper: {
+            type: String,
+            default: "/wallpapers/default.jpg",
+        },
+
+        theme: {
+            type: String,
+            default: "light",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
